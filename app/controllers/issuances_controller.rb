fc07e09d129b79f9c92db455332ff5691cb3eafd
Leopoldo Rojas Moguel <leopoldo.rojas@gmail.com>
2014-10-16 13:10:14 -0600

    Starting with Shares Issuance operation

--- app/controllers/issuances_controller.rb
+++ app/controllers/issuances_controller.rb
@@ -1,5 +1,8 @@
 class IssuancesController < ApplicationController
   def new
-  	@share_issue = ShareIssue.new
+  	@share_issue = ShareIssue.new(issue_date: Time.zone.now)
+  end
+
+  def create
   end
 end
