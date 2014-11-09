810ccbbe3ab09d338038b7ef9bb69e5b6142833c
Leopoldo Rojas Moguel <leopoldo.rojas@gmail.com>
2014-11-09 03:52:00 -0600

    Now using Bower all the way

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,16 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-# via https://github.com/sstephenson/sprockets/issues/347#issuecomment-25543201
-
-# We don't want the default of everything that isn't js or css, because it pulls too many things in
-Rails.application.config.assets.precompile.shift
-
-# Explicitly register the extensions we are interested in compiling
-Rails.application.config.assets.precompile.push(Proc.new do |path|
-  File.extname(path).in? [
-    '.html', '.erb', '.haml',                 # Templates
-    '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
-    '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
-  ]
-end)
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
