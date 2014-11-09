540444d44a8b2a075b45cae1e4766d5f207ae556
Leopoldo Rojas Moguel <leopoldo.rojas@gmail.com>
2014-11-09 14:29:28 -0600

    New try to fix assets with fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,8 +3,6 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
-
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
