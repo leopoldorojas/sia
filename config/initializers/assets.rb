efca802c3e399309334ec9a17a76f34d08564fd0
Leopoldo Rojas Moguel <leopoldo.rojas@gmail.com>
2014-11-09 03:56:28 -0600

    Change initializers with new formats as assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,15 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
+# We don't want the default of everything that isn't js or css, because it pulls too many things in
+Rails.application.config.assets.precompile.shift
+
+# Explicitly register the extensions we are interested in compiling
+Rails.application.config.assets.precompile.push(Proc.new do |path|
+  File.extname(path).in? [
+    '.html', '.erb', '.haml',                 # Templates
+    '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+    '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+  ]
+end)
