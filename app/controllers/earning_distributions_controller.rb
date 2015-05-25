6a5af304f2a9521fbb9cbbe00e2fd7434dc8fae6
Leopoldo Rojas Moguel <leopoldo.rojas@gmail.com>
2015-05-25 14:01:40 -0600

    Now the system can do a share by share holder distribution

--- app/controllers/earning_distributions_controller.rb
+++ app/controllers/earning_distributions_controller.rb
@@ -7,21 +7,22 @@ class EarningDistributionsController < ApplicationController
     previous_year = today.prev_year.year
     initial_date = Time.zone.local(previous_year, initial_date.month, initial_date.day) if initial_date >= today
 
-  	@earning_distribution = EarningDistribution.new(amount_to_distribute: 0, initial_date: initial_date, number_of_months: 12)
+  	@earning_distribution = EarningDistribution.new(initial_date: initial_date, number_of_months: 12)
   end
 
   def create
     query = query_params
     @earning_distribution = EarningDistribution.new({
-      amount_to_distribute: query['amount_to_distribute'],
+      amount_to_distribute: query['amount_to_distribute'].to_d,
       initial_date: Time.zone.local(query['initial_date(1i)'], query['initial_date(2i)'], 1),
-      number_of_months: query['number_of_months']
+      number_of_months: query['number_of_months'],
+      by_shares: (query['by_shares'] == "true")
     })
 
     respond_to do |format|
       format.html { render :new }
       format.xls { render :new }
-      format.json { render json: @share_holder_earnings.matrix }
+      format.json { render json: @earning_distribution.matrix }
     end
   end
 
@@ -29,7 +30,7 @@ class EarningDistributionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def query_params
-      params.require(:earning_distribution).permit(:amount_to_distribute, :initial_date, :number_of_months)
+      params.require(:earning_distribution).permit(:amount_to_distribute, :initial_date, :number_of_months, :by_shares, :by_earnings)
     end
 
     def authorize_for_managing
--- app/models/earning_distribution.rb
+++ app/models/earning_distribution.rb
@@ -1,12 +1,12 @@
 class EarningDistribution
   include ActiveModel::Model
 
-  attr_accessor :amount_to_distribute, :initial_date, :number_of_months, :actual_number_of_months
+  attr_accessor :amount_to_distribute, :initial_date, :number_of_months, :actual_number_of_months, :by_shares
   validates :amount_to_distribute, :initial_date, :number_of_months, presence: true
 
   def matrix
   	# Each cell must contain: ShareHolder´s Number of Shares in that date / Total number of shares in that date / Number of months to calculate
-    return false if amount_to_distribute.to_d <= 0
+    return false if amount_to_distribute.to_d <= 0 && !by_shares
   	earnings_distribution_matrix = []
   	earnings_distribution_matrix[0] = []
     list_of_dates = dates 
@@ -22,21 +22,23 @@ class EarningDistribution
 
   		ShareHolder.find_each do |share_holder|
         if month > 1
-          previous_month = month - 1
+          unless by_shares
+            previous_month = month - 1
 
-          if previous_month == (actual_number_of_months + 1)
-            earnings_for_share_holder_factor = calculate_factor(earnings_distribution_matrix, share_holder_index)
-          else 
-            last_row = earnings_distribution_matrix[previous_month].length - 1
-            shares_of_share_holder_in_previous_month = earnings_distribution_matrix[previous_month][share_holder_index][0]
-            total_shares_in_previous_month = earnings_distribution_matrix[previous_month][last_row][0]
-            return false if total_shares_in_previous_month <= 0
-            earnings_for_share_holder_factor = shares_of_share_holder_in_previous_month.to_f / total_shares_in_previous_month.to_f
+            if previous_month == (actual_number_of_months + 1)
+              earnings_for_share_holder_factor = calculate_factor(earnings_distribution_matrix, share_holder_index)
+            else 
+              last_row = earnings_distribution_matrix[previous_month].length - 1
+              shares_of_share_holder_in_previous_month = earnings_distribution_matrix[previous_month][share_holder_index][0]
+              total_shares_in_previous_month = earnings_distribution_matrix[previous_month][last_row][0]
+              return false if total_shares_in_previous_month <= 0
+              earnings_for_share_holder_factor = shares_of_share_holder_in_previous_month.to_f / total_shares_in_previous_month.to_f
+            end
+
+            earnings_for_share_holder = earnings_for_share_holder_factor / actual_number_of_months * amount_to_distribute.to_d
+            earnings_distribution_matrix[previous_month][share_holder_index][1] = earnings_for_share_holder
+            total_of_value_shares_this_month += earnings_for_share_holder
           end
-
-          earnings_for_share_holder = earnings_for_share_holder_factor / actual_number_of_months * amount_to_distribute.to_d
-          earnings_distribution_matrix[previous_month][share_holder_index][1] = earnings_for_share_holder
-          total_of_value_shares_this_month += earnings_for_share_holder
         else 
   			  earnings_distribution_matrix[0][share_holder_index] = share_holder.name
         end
@@ -55,26 +57,30 @@ class EarningDistribution
         earnings_distribution_matrix[month][share_holder_index] = []
         earnings_distribution_matrix[month][share_holder_index][0] = total_of_shares_this_month
 
-        if month > 1
+        if month > 1 && !by_shares
           earnings_distribution_matrix[month - 1][share_holder_index][1] = total_of_value_shares_this_month
         end
       end
   	end
 
-    last_index = earnings_distribution_matrix[actual_number_of_months + 1].length - 1
-    earnings_distribution_matrix[actual_number_of_months + 1][last_index] = amount_to_distribute.to_d
+    unless by_shares
+      last_index = earnings_distribution_matrix[actual_number_of_months + 1].length - 1
+      earnings_distribution_matrix[actual_number_of_months + 1][last_index] = amount_to_distribute.to_d
+    end
+    
   	matrix_by_share_holder earnings_distribution_matrix
   end
 
   private
 
     def matrix_by_share_holder matrix
+      actual_index = by_shares ? 0 : 1
       new_matrix = []
       number_of_share_holders = matrix[0].length - 1
       0.upto(number_of_share_holders) do |share_holder_index|
         new_matrix[share_holder_index] = []
         0.upto(actual_number_of_months) do |month_index|
-          new_matrix[share_holder_index][month_index] = matrix[month_index][share_holder_index].is_a?(Array) ? matrix[month_index][share_holder_index][1] : matrix[month_index][share_holder_index]
+          new_matrix[share_holder_index][month_index] = matrix[month_index][share_holder_index].is_a?(Array) ? matrix[month_index][share_holder_index][actual_index] : matrix[month_index][share_holder_index]
         end
       end
       new_matrix
