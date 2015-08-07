6f4fda71c8d41d1a7e02886511383ba5e2ec9b72
Leopoldo Rojas Moguel <leopoldo.rojas@gmail.com>
2015-08-07 16:04:56 -0600

    Progressing with Certificate layout. Now we have the proper positioning and some wrappers

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -23,7 +23,7 @@ class CertificatesController < ApplicationController
                 encoding: 'UTF-8',
                 orientation: 'Landscape',
                 page_size: 'Letter',
-                margin: { top: 15, bottom: 30, left:25, right: 25 }
+                margin: { top: 7, bottom: 15, left:20, right: 20 }
 
       end
     end
